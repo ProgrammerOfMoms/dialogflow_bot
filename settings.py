03c91e75db80a6490c9590385b035709c0b893cd
admin <admin@LAPTOP-98V9G3FJ>
2020-06-09 00:10:35 +0900

    2

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 #vk
-token = "your vk token"
+token = "fc6352b52b0aa76d13d38020e2cca2d7299c09b406f95e4af3ec46399ce76ac4d78e1e128ba2636b6916a"
 group_id = 195658194
 server_name = "longpollserver"
 
 #apiai
-ai_token = "your dialogflow token"
+ai_token = "748456792f4e46598cffa25cd83a2b42"
 lang = "ru"
 session_id = "mySessionID1"
 
